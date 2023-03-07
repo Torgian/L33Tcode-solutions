f6f5808e09e264541072162a30580f5f6de150ff
Nathan <nihongotraveler@icloud.com>
2023-03-07 12:03:34 +0800

    [javascript] Reverse Words in a string
    83ms, 48.2mb. Middle of the road.
    
    Signed-off-by: Nathan <nihongotraveler@icloud.com>

---
+++ ReverseWords.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+    const arrayOfWords = s.split(' ')
+    for (let i = 0; i < arrayOfWords.length; i++) {
+        let character = ''
+        let left = 0
+        let right = arrayOfWords[i].length - 1
+
+        word = arrayOfWords[i].split('')
+        while (left < right) {
+            character = word[right]
+            word[right] = word[left]
+            word[left] = character
+            left += 1
+            right -= 1
+        }
+        word = word.join('')
+        arrayOfWords[i] = word
+    }
+
+    return arrayOfWords.join(' ')
+
+};
+
+reverseWords("Let's take LeetCode contest")
+reverseWords("God Ding")
